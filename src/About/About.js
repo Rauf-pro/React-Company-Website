70498f8ae14b8ea6ef7af25a630d07541f4f9398
rrauf8178 <rrauf8178@gmail.com>
2022-12-29 21:00:58 +0400

    Created card-item and styled

--- src/About/About.js
+++ src/About/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./about.css";
 import about from "../../src/image/about.jpg";
+import { FcSlrBackSide, FcWorkflow, FcShop } from "react-icons/fc";
 
 const About = () => {
   return (
@@ -22,6 +23,54 @@ const About = () => {
               cupiditate eligendi, maxime velit veniam, dolores cumque hic quos
               molestiae praesentium laudantium labore magni, iusto quis!
             </p>
+            <div className="card-design">
+              <div className="about-list-item">
+                <div className="about-details-icon">
+                  <p>
+                    <FcSlrBackSide size={50} />
+                  </p>
+                </div>
+                <div>
+                  <h6>Perfect day out</h6>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Magnam, ipsam?
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="card-design">
+              <div className="about-list-item">
+                <div className="about-details-icon">
+                  <p>
+                    <FcWorkflow size={50} />
+                  </p>
+                </div>
+                <div>
+                  <h6>Perfect day out</h6>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Magnam, ipsam?
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="card-design">
+              <div className="about-list-item">
+                <div className="about-details-icon">
+                  <p>
+                    <FcShop size={50} />
+                  </p>
+                </div>
+                <div>
+                  <h6>Perfect day out</h6>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Magnam, ipsam?
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
