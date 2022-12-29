4f17c5cde8458a0ffc2cb6851a28fac40a63ced8
rrauf8178 <rrauf8178@gmail.com>
2022-12-29 12:51:40 +0400

    adding new text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App is Navbar branch</div>
-  )
-}
+    <>
+      <div>App is Navbar branch</div>
+      <div>Different section</div>
+      <div>Mobile view</div>
+    </>
+  );
+};
 
-export default App
+export default App;
