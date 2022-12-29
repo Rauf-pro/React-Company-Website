939a97ac27c9c00e589afe7696bb43fb7199968d
rrauf8178 <rrauf8178@gmail.com>
2022-12-29 22:07:47 +0400

    Finished Service section and styling

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./service.css";
+import { FcOk } from "react-icons/fc";
 
 const Service = () => {
   const servicedata = [
@@ -11,54 +12,54 @@ const Service = () => {
       service4: "Digital Marketing",
     },
     {
-        id: 2,
-        service1: "Web Development",
-        service2: "Mobile App Development",
-        service3: "UI/UX Design",
-        service4: "Digital Marketing",
-      },
-      {
-        id: 3,
-        service1: "Web Development",
-        service2: "Mobile App Development",
-        service3: "UI/UX Design",
-        service4: "Digital Marketing",
-      },
-      {
-        id: 4,
-        service1: "Web Development",
-        service2: "Mobile App Development",
-        service3: "UI/UX Design",
-        service4: "Digital Marketing",
-      },
-      {
-        id: 5,
-        service1: "Web Development",
-        service2: "Mobile App Development",
-        service3: "UI/UX Design",
-        service4: "Digital Marketing",
-      },
-      {
-        id: 6,
-        service1: "Web Development",
-        service2: "Mobile App Development",
-        service3: "UI/UX Design",
-        service4: "Digital Marketing",
-      },
-      {
-        id: 7,
-        service1: "Web Development",
-        service2: "Mobile App Development",
-        service3: "UI/UX Design",
-        service4: "Digital Marketing",
-      },
-      {
-        id: 8,
-        service1: "Web Development",
-        service2: "Mobile App Development",
-        service3: "UI/UX Design",
-        service4: "Digital Marketing",
-      },
+      id: 2,
+      service1: "Web Development",
+      service2: "Mobile App Development",
+      service3: "UI/UX Design",
+      service4: "Digital Marketing",
+    },
+    {
+      id: 3,
+      service1: "Web Development",
+      service2: "Mobile App Development",
+      service3: "UI/UX Design",
+      service4: "Digital Marketing",
+    },
+    {
+      id: 4,
+      service1: "Web Development",
+      service2: "Mobile App Development",
+      service3: "UI/UX Design",
+      service4: "Digital Marketing",
+    },
+    {
+      id: 5,
+      service1: "Web Development",
+      service2: "Mobile App Development",
+      service3: "UI/UX Design",
+      service4: "Digital Marketing",
+    },
+    {
+      id: 6,
+      service1: "Web Development",
+      service2: "Mobile App Development",
+      service3: "UI/UX Design",
+      service4: "Digital Marketing",
+    },
+    {
+      id: 7,
+      service1: "Web Development",
+      service2: "Mobile App Development",
+      service3: "UI/UX Design",
+      service4: "Digital Marketing",
+    },
+    {
+      id: 8,
+      service1: "Web Development",
+      service2: "Mobile App Development",
+      service3: "UI/UX Design",
+      service4: "Digital Marketing",
+    },
   ];
 
   return (
@@ -70,12 +71,37 @@ const Service = () => {
         <span className="line"></span>
       </div>
       <div className="row">
-        {servicedata.map((item, index) =>(
-            <>
-                <div className="col-xl-3 col-lg-3 col-md-6 col-sm-12">
-                    
-                </div>
-            </>
+        {servicedata.map((item, index) => (
+          <div className="col-xl-3 col-lg-3 col-md-6 col-sm-12" key={index}>
+            <div className="services">
+              <span className="service-number">
+                <p>{item.id}</p>
+              </span>
+
+              <div className="list-of-service">
+                <p>
+                  <FcOk size={20} style={{ margin: "5px" }} />
+                  {item.service1}
+                </p>
+                <span className="service_item_underline"></span>
+                <p>
+                  <FcOk size={20} style={{ margin: "5px" }} />
+                  {item.service2}
+                </p>
+                <span className="service_item_underline"></span>
+                <p>
+                  <FcOk size={20} style={{ margin: "5px" }} />
+                  {item.service3}
+                </p>
+                <span className="service_item_underline"></span>
+                <p>
+                  <FcOk size={20} style={{ margin: "5px" }} />
+                  {item.service4}
+                </p>
+              
+              </div>
+            </div>
+          </div>
         ))}
       </div>
     </div>
