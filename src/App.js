ede15a3e1fa4a2263eacc4fe2dc1f19debc7e8f3
rrauf8178 <rrauf8178@gmail.com>
2022-12-30 18:26:55 +0400

    Added React-Scroll-To-Top and custom styling

--- src/App.js
+++ src/App.js
@@ -5,16 +5,25 @@ import About from "./components/About/About";
 import Service from "./components/Service/Service";
 import Testimonial from "./components/Testimonial/Testimonial";
 import Subscription from "./components/Subscription/Subscription";
+import ScrollToTop from "react-scroll-to-top";
 
 const App = () => {
   return (
     <>
-      <Navbar/>
-      <Home/>
-      <About/>
-      <Service/>
-      <Testimonial/>
-      <Subscription/>
+      <Navbar />
+      <Home />
+      <About />
+      <Service />
+      <Testimonial />
+      <Subscription />
+
+      <ScrollToTop
+      color="white"
+        smooth
+        height="20"
+        width="20"
+        style={{ borderRadius: "90px", backgroundColor: "#38004c" }}
+      />
     </>
   );
 };
